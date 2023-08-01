e09af96d2503749be8f1e98df1b5186fcc528109
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-08-01 15:03:41 -0500

    added routes, controller methods, user controllers

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,39 @@
 class OpportunitiesController < ApplicationController
+  def index
+    @opportunies = Opportunity.all
+    render :index
+  end
+
+  def show
+    @opportunity = Opportunity.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @opportunity = Opportunity.create(
+      title: params[:title],
+      description: params[:description],
+      date: params[:date],
+      user_id: params[:user_id],
+      status: params[:status],
+    )
+    render :show
+  end
+
+  def update
+    @opportunity = Opportunity.update(
+      title: params[:title] || @opportunity.title,
+      description: params[:description] || @opportunity.description,
+      date: params[:date] || @opportunity.date,
+      # user_id: params[:user_id] || @opportunity.user_id,
+      status: params[:status] || @opportunity.status,
+    )
+    render :show
+  end
+
+  def destroy
+    @opportunity = Opportunity.find_by(id: params[:id])
+    @opportunity.destroy
+    render json: { message: "Opportunity lost" } #google this!!!
+  end
 end
