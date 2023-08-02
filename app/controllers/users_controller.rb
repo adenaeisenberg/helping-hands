0cb998fc36c60576b9981496c30469e7d767723f
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-08-02 12:55:15 -0400

    adds password attribute to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,13 @@ class UsersController < ApplicationController
   def create
     @user = User.create(
       name: params[:name],
+      email: params[:email],
+      password: params[:password],
       bio: params[:bio],
       DOB: params[:DOB],
       phone_number: params[:phone_number],
-      email: params[:email],
       admin: params[:admin],
+
     )
     render :show
   end
@@ -24,10 +26,11 @@ class UsersController < ApplicationController
   def update
     @user = User.update(
       name: params[:name] || @user.name,
+      email: params[:email] || @user.email,
+      password: params[:password] || @user.password,
       bio: params[:bio] || @user.bio,
       DOB: params[:DOB] || @user.dob,
       phone_number: params[:phone_number] || @user.phone_number,
-      email: params[:email] || @user.email,
       admin: params[:admin] || @user.admin, #only admin
     )
     render :show
