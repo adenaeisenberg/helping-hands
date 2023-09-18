9b8948df7f66f151d659a8221f59ecaa90bacac1
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-09-18 14:38:05 -0500

    add cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  # before_action authenticate: current_user
   has_many :opportunities
 end
